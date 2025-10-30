6f8e6a8724046885b3e8bfb23b475d528bdbbcae
Daniel Olguin <danieldavidolguin@gmail.com>
2025-10-30 14:52:10 -0300

    Corrijo la definición de la lista de argumentos de las funciones para que almacenen objetos tipo Var, no ID (que son genéricos)

--- src/main/python/tablaDeSimbolos/Funcion.py
+++ src/main/python/tablaDeSimbolos/Funcion.py
@@ -1,12 +1,13 @@
 from typing import List
 from ID import ID
+from Variable import Variable
 
 class Funcion(ID) :
     # args = [] # Esto está mal acá porque se define como una variable de clase pero lo que necesitamos es una var de instancia
 
     def __init__(self):
         super().__init__()
-        self.args: List[ID] = []
+        self.args: List[Variable] = []
 
-    def getListaArgs(self) -> List[ID]:
+    def getListaArgs(self) -> List[Variable]:
         return self.args

8be853767b8bed581aed4d52c11df83d6489663f
Daniel Olguin <danieldavidolguin@gmail.com>
2026-01-14 11:56:14 -0300

    Agrego la creación básica de símbolos tipo variable y una primera aproximación al control de incializadores. Reorganizo y comento métodos obsoletos.

--- src/main/python/Escucha.py
+++ src/main/python/Escucha.py
@@ -29,19 +29,25 @@ class Escucha(compiladorListener):
         self.huboErrores = True
         print(f"ERROR {tipo}: {msj}")
 
-    def buscarFactorCores(self, ctx: ParserRuleContext):
-        """Recorre recursivamente el subárbol sintáctico a partir del contexto que se le pase y devuelve una lista con todos los nodos FactorCoreContext encontrados."""
-        result = [] # Acumula los FactorCores encontrados
-        # Paso 1: verificamos si el context actual es factorCore
-        if isinstance(ctx, compiladorParser.FactorCoreContext):
-            result.append(ctx) # Si encontramos un FactorCore, lo agregamos a la lista
-        # Paso 2: recorremos todos los hijos del context
-        for child in ctx.getChildren():
-            if isinstance(child, ParserRuleContext):
-                # Llamada recursiva: exploramos los descendientes y nos traemos los FactorCores que encontremos
-                result.extend(self.buscarFactorCores(child)) # Extend fusiona listas elemento a elemento
-        # Paso 3: devolvemos la lista de factorCore encontrados
-        return result
+    def obtenerTipoDato(self, expresion: str) -> str:
+        pass
+
+    def tiposCompatibles(self, tipo1: str, tipo2: str) -> bool:
+        pass
+    
+    # def buscarFactorCores(self, ctx: ParserRuleContext):
+    #     """Recorre recursivamente el subárbol sintáctico a partir del contexto que se le pase y devuelve una lista con todos los nodos FactorCoreContext encontrados."""
+    #     result = [] # Acumula los FactorCores encontrados
+    #     # Paso 1: verificamos si el context actual es factorCore
+    #     if isinstance(ctx, compiladorParser.FactorCoreContext):
+    #         result.append(ctx) # Si encontramos un FactorCore, lo agregamos a la lista
+    #     # Paso 2: recorremos todos los hijos del context
+    #     for child in ctx.getChildren():
+    #         if isinstance(child, ParserRuleContext):
+    #             # Llamada recursiva: exploramos los descendientes y nos traemos los FactorCores que encontremos
+    #             result.extend(self.buscarFactorCores(child)) # Extend fusiona listas elemento a elemento
+    #     # Paso 3: devolvemos la lista de factorCore encontrados
+    #     return result
 
     # ###########################################################################
     # Inicio
@@ -58,7 +64,7 @@ class Escucha(compiladorListener):
         # self.buscarVariablesNoUsadas()
     
         # Tenemos que imprimir la TS solo si no hubo errores sintácticos ni semánticos
-        if self.huboErrores and EscuchaErroresSintacticos.errores: # Lista NO vacía = True
+        if self.huboErrores or EscuchaErroresSintacticos.errores: # Lista NO vacía = True
             with open("ContenidoTS.txt", "w") as f:
                 f.write("Imposible generar la TS: Se encontraron errores durante el parsing.\n")
         else: # Si no hubieron errores, imprimimos la TS
@@ -98,15 +104,55 @@ class Escucha(compiladorListener):
             # Si hay un error de sintaxis en la declaración, no tiene sentido seguir
             return
         
-    def exitOpal(self, ctx: compiladorParser.OpalContext):
-        factor_cores = self.buscarFactorCores(ctx)
-        for fc in factor_cores:
-            print("Factor encontrado:", fc.getText())
+        # ----------- Lectura de la instrucción -----------
+        # 1ro) Extraemos el tipo de dato, que es el mismo para todas las declaraciones en la instrucción
+        tipo_dato = ctx.tipo().getText() 
+        # 2do) Convertimos el texto plano de las declaraciones en una lista de declaraciones individuales (Ej: ["x=5", "y", "z=x+2"])
+        declaraciones = ctx.getText().replace(tipo_dato,'').replace(';','').strip()
+        declaraciones = [declaracion.strip() for declaracion in declaraciones.split(',')]
+
+        #  ----------- Procesamiento de las declaraciones y generación de símbolos -----------
+        for declaracion in declaraciones:
+            # 1ro) Extraemos los datos de la declaración
+            if '=' in declaracion: # Tipo 1: con inicialización
+                nombre, inicializador = [term.strip() for term in declaracion.split('=')]
+                inicializada = True
+            else: # Tipo 2: sin inicialización
+                nombre = declaracion
+                inicializada = False
+            
+            # 2do) Vemos si el símbolo ya existe en el contexto actual
+            if(self.ts.buscarSimboloContexto(nombre)):
+                self.registrarError(TipoError.SEMANTICO, f"La variable '{nombre}' ya fue declarada en este contexto.")
+                continue # Pasamos a la siguiente sin agregar nada a la TS
+            
+            # # 3ro) Controlamos el inicializador
+            # if(inicializada):
+            #     # 3.1) Chequeamos la existencia de variables usadas en el inicializador
+
+            #     # 3.2) Chequeamos compatibilidad de tipos
+            #     tipo_inicializador = self.obtenerTipoDato(inicializador)
+            #     if not self.tiposCompatibles(tipo_dato, tipo_inicializador):
+            #         self.registrarError(TipoError.SEMANTICO, f"Incompatibilidad de tipos al inicializar la variable '{nombre}'. Se esperaba un valor de tipo '{tipo_dato}' pero se recibió '{tipo_inicializador}'.")
+            #         continue # Pasamos a la siguiente sin agregar nada a la TS
+            
+            # 4to) Creamos el símbolo y lo integramos a la TS
+            nueva_variable = Variable(nombre, tipo_dato)
+            if(inicializada):
+                nueva_variable.setInicializado()
+            self.ts.addSimbolo(nueva_variable)
 
     # ------------ Agregado de símbolos tipo Funcion ------------
-        # ToDo - No entra al TP3
+    # Funcion: (nombre, tipoDato, args: Optional, inicializado, usado)
+        
+    def exitPrototipo(self, ctx: compiladorParser.PrototipoContext):
+        pass
 
     # ###########################################################################
     # Otros chequeos de semántica
     # ###########################################################################
 
+    # def exitOpal(self, ctx: compiladorParser.OpalContext):
+    #     factor_cores = self.buscarFactorCores(ctx)
+    #     for fc in factor_cores:
+    #         print("Factor encontrado:", fc.getText())

fc6a2273ec9ad9a66557a69865be6298f894145b
Daniel Olguin <danieldavidolguin@gmail.com>
2026-02-19 16:16:46 -0300

    Agrego el optimizador al proyecto.

--- src/main/python/App.py
+++ src/main/python/App.py
@@ -5,6 +5,7 @@ from compiladorParser import compiladorParser
 from Escucha import Escucha
 from EscuchaErroresSintacticos import EscuchaErroresSintacticos
 from Caminante import Caminante
+from Optimizador import Optimizador
 
 # En caso de no poder ejecutar el programa Python por
 # problemas de version (error ATNdeserializer), se
@@ -17,8 +18,8 @@ def main(argv):
     # Entradas de testing
     # archivo = "input/testerFunciones.txt"
     # archivo = "input/testerErroresSintacticos.txt"
-    archivo = "input/entradaSimple.txt"
-    # archivo = "input/entradaCorrecta.txt"
+    # archivo = "input/entradaSimple.txt"
+    archivo = "input/entradaCorrecta.txt"
     # archivo = "input/entradaConErrores.txt"
 
     if len(argv) > 1 :
@@ -57,16 +58,21 @@ def main(argv):
             for linea in visitante.codigoIntermedio:
                 f.write(linea + "\n")
 
+        # Optimización del código intermedio
+        optimizador = Optimizador()
+        optimizador.optimizar("output/CodigoIntermedio.txt")
+
         # Impresión de código optimizado
-        # TODO: Implementar optimizaciones y generar un archivo de salida para el código optimizado
+        optimizador.imprimir_codigo_optimizado("output/CodigoOptimizado.txt")
 
     else: # Si hubo errores, limpiamos los archivos de salida para evitar confusiones
         print("Entrada incorrecta. Limpiando archivos de salida...")
         with open("output/ContenidoTS.txt", "w") as f:
-            f.write("Imposible generar la TS: Se encontraron errores durante el parsing.\n")
+            pass # Limpia el archivo de la TS
         with open("output/CodigoIntermedio.txt", "w") as f:
-            f.write("Imposible generar el código intermedio: Se encontraron errores durante el parsing.\n")
-        # TODO limpiar el archivo de código optimizado también, una vez que se implemente la generación de código optimizado
+            pass # Limpia el archivo de código intermedio
+        with open("output/CodigoOptimizado.txt", "w") as f:
+            pass # Limpia el archivo de código optimizado
 
 if __name__ == '__main__':
     main(sys.argv)
---
+++ src/main/python/Optimizador.py
@@ -0,0 +1,48 @@
+class Optimizador:
+    def __init__(self):
+        self.codigo = []
+
+    def optimizar(self, archivo_entrada):
+        """Recibe la dirección del archivo de código intermedio, lo carga y aplica optimizaciones iterativamente hasta que no se puedan hacer más cambios."""
+
+        if not self.cargar_codigo(archivo_entrada): # Lista vacía <=> Archivo vacío o no encontrado
+            print("No hay código para optimizar.")
+            return
+        
+        print("OPTIMIZANDO CÓDIGO INTERMEDIO...")
+        iteracion = 1
+        while True:
+            print(f"Iteración {iteracion}")
+            resultado1 = self.plegado_constantes()
+            resultado2 = self.propagacion_copia()
+            resultado3 = self.eliminacion_codigo_muerto()
+
+            if not (resultado1 or resultado2 or resultado3): # Si no se hicieron cambios, terminamos
+                print("No se realizaron más cambios. Terminando optimización...")
+                break
+            iteracion += 1
+
+    def cargar_codigo(self, archivo_entrada):
+        try:
+            with open(archivo_entrada, "r") as f:
+                    self.codigo = [linea.strip() for linea in f.readlines()]
+            return True
+        except FileNotFoundError:
+            self.codigo = []
+            print(f"ERROR: '{archivo_entrada}' no encontrado.")
+            return False
+
+    def imprimir_codigo_optimizado(self, archivo_salida):
+        with open(archivo_salida, "w") as f:
+            for linea in self.codigo:
+                f.write(linea + "\n")
+        print(f"Código optimizado guardado en '{archivo_salida}'.")
+
+    # ---------------- Plegado de constantes ----------------
+    # Se evalúan expresiones constantes en tiempo de compilación y se reemplazan por su resultado
+
+    # ---------------- Propagación de copia ----------------
+    # Se eliminan temporales intermedias cuando es seguro
+
+    # ---------------- Eliminación de código muerto ----------------
+    # Se eliminan instrucciones que no afectan el resultado del programa, como asignaciones a variables que nunca se usan
